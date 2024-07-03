1988cba0ed5eddc0febcbd07ea9a22e9bfa92616
RobertsRob <83211059+RobertsRob@users.noreply.github.com>
2024-07-03 15:54:21 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 def check_sum(num1, num2):
     if num1 <= 0 and num2 <= 0:
         return "Числа негативные или равны нулю"
-    elif num1 == num2:
+    if num1 == num2:
         return "Числа равны друг другу"
+
+    sum_result = num1 + num2
+    if sum_result >= 10:
+        return str(sum_result)
     else:
-        sum_result = num1 + num2
-        if sum_result >= 10:
-            return str(sum_result)
-        else:
-            return "Сумма чисел слишком мала"
+        return "Сумма чисел слишком мала"
 
 print(check_sum(5, 6))    # Выведет: "11"
 print(check_sum(3, 2))    # Выведет: "Сумма чисел слишком мала"
